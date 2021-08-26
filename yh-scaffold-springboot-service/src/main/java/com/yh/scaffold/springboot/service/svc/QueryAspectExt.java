471be49b26a805adb22e00943f4a5753104e5f16
zhanglei <zhanglei10@yonghui.cn>
2021-08-26 20:42:09 +0800

    add files

---
+++ yh-scaffold-springboot-service/src/main/java/com/yh/scaffold/springboot/service/svc/QueryAspectExt.java
@@ -0,0 +1,11 @@
+package com.yh.scaffold.springboot.service.svc;
+
+import com.yh.common.lark.orm.aop.QueryAspect;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class QueryAspectExt extends QueryAspect {
+
+}

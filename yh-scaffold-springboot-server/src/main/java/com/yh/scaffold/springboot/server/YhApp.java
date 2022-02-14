675dcb4c5522128ac0a1a7bbf61731ab2fde651d
Alex <alexanderlc@126.com>
2022-02-14 19:24:10 +0800

    Updated dao layer impl logic.

--- yh-scaffold-springboot-server/src/main/java/com/yh/scaffold/springboot/server/YhApp.java
+++ yh-scaffold-springboot-server/src/main/java/com/yh/scaffold/springboot/server/YhApp.java
@@ -1,10 +1,8 @@
 package com.yh.scaffold.springboot.server;
 
 import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
